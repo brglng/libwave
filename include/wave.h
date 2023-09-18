aa142accb047bed6eef862f3e5bb9ec31a1b4576
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2023-09-18 12:32:08 +0800

    Fix link error

--- include/wave.h
+++ include/wave.h
@@ -31,13 +31,18 @@ extern "C" {
 #define WAVE_RESTRICT __restrict
 #endif
 
+#if defined(_MSV_VER)
+    #define WAVE_API __declspec(dllexport)
+#else
+    #define WAVE_API __attribute__((visibility("default")))
+#endif
+
 #if defined(__APPLE__) || defined(_MSC_VER)
 typedef long long           WaveI64;
 typedef unsigned long long  WaveU64;
 typedef long long           WaveIntPtr;
 typedef unsigned long long  WaveUIntPtr;
-#else
-#if defined(_WIN64) || defined(__x86_64) || defined(__amd64)
+#elif defined(_WIN64) || defined(__x86_64) || defined(__amd64)
 typedef long                WaveI64;
 typedef unsigned long       WaveU64;
 typedef long                WaveIntPtr;
@@ -47,7 +52,6 @@ typedef long long           WaveI64;
 typedef unsigned long long  WaveU64;
 typedef int                 WaveIntPtr;
 typedef unsigned int        WaveUIntPtr;
-#endif
 #endif
 
 #if defined(__cplusplus) && __cplusplus >= 201103L
@@ -111,8 +115,8 @@ char* wave_strndup(WAVE_CONST char *str, size_t n);
 int wave_vasprintf(char **str, WAVE_CONST char *format, va_list args);
 int wave_asprintf(char **str, WAVE_CONST char *format, ...);
 
-WAVE_CONST WaveErr* wave_err(void);
-void wave_err_clear(void);
+WAVE_API WAVE_CONST WaveErr* wave_err(void);
+WAVE_API void wave_err_clear(void);
 
 #define WAVE_OPEN_READ       1
 #define WAVE_OPEN_WRITE      2
@@ -126,9 +130,9 @@ typedef struct _WaveFile WaveFile;
  *  @param mode         The mode for open (same as {fopen})
  *  @return             NULL if the memory allocation for the {WaveFile} object failed. Non-NULL means the memory allocation succeeded, but there can be other errors, which can be obtained using {wave_errno} or {wave_error}.
  */
-WaveFile* wave_open(WAVE_CONST char* filename, WaveU32 mode);
-void     wave_close(WaveFile* self);
-WaveFile* wave_reopen(WaveFile* self, WAVE_CONST char* filename, WaveU32 mode);
+WAVE_API WaveFile* wave_open(WAVE_CONST char* filename, WaveU32 mode);
+WAVE_API void     wave_close(WaveFile* self);
+WAVE_API WaveFile* wave_reopen(WaveFile* self, WAVE_CONST char* filename, WaveU32 mode);
 
 /** Read a block of samples from the wav file
  *
@@ -138,7 +142,7 @@ WaveFile* wave_reopen(WaveFile* self, WAVE_CONST char* filename, WaveU32 mode);
  *  @return             The number of frames read. If returned value is less than {count}, either EOF reached or an error occured
  *  @remarks            This API does not support extensible format. For extensible format, use {wave_read_raw} instead.
  */
-size_t wave_read(WaveFile* self, void *buffer, size_t count);
+WAVE_API size_t wave_read(WaveFile* self, void *buffer, size_t count);
 
 /** Write a block of samples to the wav file
  *
@@ -148,26 +152,26 @@ size_t wave_read(WaveFile* self, void *buffer, size_t count);
  *  @return         The number of frames written. If returned value is less than {count}, either EOF reached or an error occured.
  *  @remarks        This API does not support extensible format. For extensible format, use {wave_read_raw} instead.
  */
-size_t wave_write(WaveFile* self, WAVE_CONST void *buffer, size_t count);
+WAVE_API size_t wave_write(WaveFile* self, WAVE_CONST void *buffer, size_t count);
 
 /** Tell the current position in the wav file.
  *
  *  @param self     The pointer to the WaveFile structure.
  *  @return         The current frame index.
  */
-long int wave_tell(WAVE_CONST WaveFile* self);
+WAVE_API long int wave_tell(WAVE_CONST WaveFile* self);
 
-int  wave_seek(WaveFile* self, long int offset, int origin);
-void wave_rewind(WaveFile* self);
+WAVE_API int  wave_seek(WaveFile* self, long int offset, int origin);
+WAVE_API void wave_rewind(WaveFile* self);
 
 /** Tell if the end of the wav file is reached.
  *
  *  @param self     The pointer to the WaveFile structure.
  *  @return         Non-zero integer if the end of the wav file is reached, otherwise zero.
  */
-int wave_eof(WAVE_CONST WaveFile* self);
+WAVE_API int wave_eof(WAVE_CONST WaveFile* self);
 
-int wave_flush(WaveFile* self);
+WAVE_API int wave_flush(WaveFile* self);
 
 /** Set the format code
  *
@@ -175,7 +179,7 @@ int wave_flush(WaveFile* self);
  *  @param format   The format code, which should be one of `WAVE_FORMAT_*`
  *  @remarks        All data will be cleared after the call. {wave_errno} can be used to get the error code if there is an error.
  */
-void wave_set_format(WaveFile* self, WaveU16 format);
+WAVE_API void wave_set_format(WaveFile* self, WaveU16 format);
 
 /** Set the number of channels
  *
@@ -183,7 +187,7 @@ void wave_set_format(WaveFile* self, WaveU16 format);
  *  @param num_channels     The number of channels
  *  @remarks                All data will be cleared after the call. {wave_errno} can be used to get the error code if there is an error.
  */
-void wave_set_num_channels(WaveFile* self, WaveU16 num_channels);
+WAVE_API void wave_set_num_channels(WaveFile* self, WaveU16 num_channels);
 
 /** Set the sample rate
  *
@@ -191,7 +195,7 @@ void wave_set_num_channels(WaveFile* self, WaveU16 num_channels);
  *  @param sample_rate      The sample rate
  *  @remarks                All data will be cleared after the call. {wave_errno} can be used to get the error code if there is an error.
  */
-void wave_set_sample_rate(WaveFile* self, WaveU32 sample_rate);
+WAVE_API void wave_set_sample_rate(WaveFile* self, WaveU32 sample_rate);
 
 /** Get the number of valid bits per sample
  *
@@ -199,7 +203,7 @@ void wave_set_sample_rate(WaveFile* self, WaveU32 sample_rate);
  *  @param bits     The value of valid bits to set
  *  @remarks        If {bits} is 0 or larger than 8*{sample_size}, an error will occur. All data will be cleared after the call. {wave_errno} can be used to get the error code if there is an error.
  */
-void wave_set_valid_bits_per_sample(WaveFile* self, WaveU16 bits);
+WAVE_API void wave_set_valid_bits_per_sample(WaveFile* self, WaveU16 bits);
 
 /** Set the size (in bytes) per sample
  *
@@ -207,16 +211,16 @@ void wave_set_valid_bits_per_sample(WaveFile* self, WaveU16 bits);
  *  @param sample_size      Number of bytes per sample
  *  @remarks                When this function is called, the {BitsPerSample} and {ValidBitsPerSample} fields in the wav file will be set to 8*{sample_size}. All data will be cleared after the call. {wave_errno} can be used to get the error code if there is an error.
  */
-void wave_set_sample_size(WaveFile* self, size_t sample_size);
+WAVE_API void wave_set_sample_size(WaveFile* self, size_t sample_size);
 
-WaveU16 wave_get_format(WAVE_CONST WaveFile* self);
-WaveU16 wave_get_num_channels(WAVE_CONST WaveFile* self);
-WaveU32 wave_get_sample_rate(WAVE_CONST WaveFile* self);
-WaveU16 wave_get_valid_bits_per_sample(WAVE_CONST WaveFile* self);
-size_t wave_get_sample_size(WAVE_CONST WaveFile* self);
-size_t wave_get_length(WAVE_CONST WaveFile* self);
-WaveU32 wave_get_channel_mask(WAVE_CONST WaveFile* self);
-WaveU16 wave_get_sub_format(WAVE_CONST WaveFile* self);
+WAVE_API WaveU16 wave_get_format(WAVE_CONST WaveFile* self);
+WAVE_API WaveU16 wave_get_num_channels(WAVE_CONST WaveFile* self);
+WAVE_API WaveU32 wave_get_sample_rate(WAVE_CONST WaveFile* self);
+WAVE_API WaveU16 wave_get_valid_bits_per_sample(WAVE_CONST WaveFile* self);
+WAVE_API size_t wave_get_sample_size(WAVE_CONST WaveFile* self);
+WAVE_API size_t wave_get_length(WAVE_CONST WaveFile* self);
+WAVE_API WaveU32 wave_get_channel_mask(WAVE_CONST WaveFile* self);
+WAVE_API WaveU16 wave_get_sub_format(WAVE_CONST WaveFile* self);
 
 #ifdef __cplusplus
 }
